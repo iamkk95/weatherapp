f8df03e963b61a8e5a93067020b880b604621f23
Kailash Babu <37300510+iamkk95@users.noreply.github.com>
2022-03-11 11:47:00 +0530

    update

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ class App extends React.Component{
 
     if(city && country){
       const api_call = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`)
+        `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`)
         const response = await api_call.json();
         console.log(response);
     
